8282bf0771e1358c87bdeed7bd3c0824122cd775
nopeless <38830903+nopeless@users.noreply.github.com>
2023-10-07 13:35:35 -0500

    remove .nuxt

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/hudsonx86/Desktop/_/PROGRAMZ/weather/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- .nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = string
-declare module "/Users/hudsonx86/Desktop/_/PROGRAMZ/weather/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
